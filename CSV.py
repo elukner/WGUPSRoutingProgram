a81d0fd51b85592b356d6770dc1038dbc8904b8f
elukner <elukner@gmail.com>
2024-11-06 15:28:08 -0700

    Added todo comments from part C using steps from the C950 WGUPS Project Implementation Steps - Example - Nearest Neighbor document into Truck.py and CSV.py.

--- CSV.py
+++ CSV.py
@@ -2,5 +2,16 @@
 # supporting functions distanceBetween(), minDistanceFrom(), and timeToDeliver()
 
 #TODO distanceBetween()
+# C.1) Function to return the distance between two addresses:
+# 8-Define distanceBetween(address1, address2)
+# 9-Return distanceData[addressData.index(address1)][addressData.index(address2)]
+# i.e. distances between addresses can be accessed via distanceData[i][j];
+
+
 #TODO minDistanceFrom()
+# C.2) Function to find min distance/address:
+# 10-Define minDistanceFrom(fromAddress, truckPackages)
+# 11-Return min distance address to fromAddress
+# i.e. call distanceBetween(address1, address2) in a loop for all the addresses in the Truck
+
 #TODO timeToDeliver()
--- Truck.py
+++ Truck.py
@@ -1,1 +1,18 @@
-#TODO 2-Create Package and Truck objects and have packageCSV and distanceCSV and addressCSV files ready
+#TODO 2-Create Package and Truck objects
+# and have packageCSV and distanceCSV and addressCSV files ready
+
+#TODO + truckID
+#TODO + packages: List[Package]
+
+#TODO + truckLoadPackages(package?<-this is not in implementation steps)
+# C.3) Function to load packages into Trucks:
+# 12-Define truckLoadPackages()
+# 13-Load Trucks based on assumptions provided
+# (ex. Truck-2 must have some packages, some packages go together,
+# some packages are delayed, ...)
+# 14-And closest addresses/packages until there is 16 packages in a Truck
+# i.e. Load manually/heuristically or Loop package addresses
+# and call minDistanceFrom(fromAddress, truckPackages)
+# for all the addresses in the Truck not visited yet
+
+#TODO + deliverPackage()
