6ad38ca2014da13a53e4e9bbd77ae2e4adad8d1c
elukner <elukner@gmail.com>
2024-11-06 20:20:16 -0700

    Finished skeleton code for Package.py.

--- Package.py
+++ Package.py
@@ -10,3 +10,24 @@
 #TODO + status
 #TODO + deliveryTime
 #TODO + updateStatus(status)
+class Package:
+    def __init__(self, packageID, address, city, state, zip, deliveryDeadline,
+                 massKilo, pageSpecialNotes, status= "At Hub", deliveryTime = None):
+
+        self.packageID = packageID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deliveryDeadline = deliveryDeadline
+        self.massKilo = massKilo
+        self.pageSpecialNotes = pageSpecialNotes
+        self.status = status  # Initial status
+        self.deliveryTime = deliveryTime  # Time when the package is delivered
+
+    # TODO + updateStatus(status)
+    def updateStatus(self,status):
+        pass #TODO delete later
+
+    def __str__(self):
+        pass #TODO delete later
