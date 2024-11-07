e307169df07c849f4b70f98a1af342df4729d609
elukner <elukner@gmail.com>
2024-11-06 20:25:51 -0700

    Finished skeleton code for main.py.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from HashTable import *
+
 #TODO loadPackageData(HashTable)
 #TODO 3-Create loadPackageData(HashTable) to
 # - read packages from packageCSV file (see C950 - Webinar-2 - Getting Greedy, who moved my data  webinar)
 # - update Package object
 # - insert Package object into HashTable with the key=PackageID and Item=Package
+def loadPackageData(HashTable):
+    pass #TODO delete later
 
 #TODO loadDistanceData(distanceData)
 # B.1) Upload Distances:
@@ -11,6 +15,8 @@
 # - read distances from distanceCSV file; row by row
 # - append row to distanceData (two-dimensional list.
 # See C950 WGUPS Distance Table Matrix)
+def loadDistanceData(distanceData):
+    pass #TODO delete later
 
 #TODO loadAddressData(addressData)
 # B.2) Upload Addresses:
@@ -18,6 +24,8 @@
 # 7-Define loadAddressData(addressData) to read addressCSV file
 # - read only addresses from addressCSV file
 # - append address to addressData.
+def loadAddressData(addressData):
+    pass #TODO delete later
 
 #TODO truckLoadPackages() ?? does this function need to be here or only in truck object
 #TODO truckDeliverPackages(truck) ?? does this function need to be here or only in truck object
@@ -31,7 +39,13 @@
 # 3. Get All Package Status with a Time
 # 4. Exit the Program
 # ***************************************
-
+def printUI():
+    print(f'***************************************')
+    print(f'1. Print All Package Status and Total Mileage')
+    print(f'2. Get a Single Package Status with a Time')
+    print(f'3. Get All Package Status with a Time')
+    print(f'4. Exit the Program')
+    print(f'***************************************')
 
 # This is a sample Python script.
 
@@ -46,6 +60,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    printUI()
+    #print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
