d3b36f9d6ab3d23ede7354aa2bdfd63472bd93ce
elukner <elukner@gmail.com>
2024-11-06 21:01:17 -0700

    Updated comments in HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -24,6 +24,15 @@ class HashTable:
         pass #TODO delete later
 
 #TODO + lookUp(self, key)
+# B.  Develop a look-up function that takes the package ID as input and returns
+# each of the following corresponding data components:
+# delivery address
+# delivery deadline
+# delivery city
+# delivery zip code
+# package weight
+# delivery status (i.e., at the hub, en route, or delivered), including the delivery time
+
     def lookUp(self, key):
         pass #TODO delete later
 
