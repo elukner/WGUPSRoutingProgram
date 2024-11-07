2140f9828caba30e543b5182ac4bcd531cb77e32
elukner <elukner@gmail.com>
2024-11-06 23:06:13 -0700

    Added test functions to testPackage.py

--- tests/testPackage.py
+++ tests/testPackage.py
@@ -1,8 +1,12 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+class PackageTestCase(unittest.TestCase):
+    def test__init__(self):
+        self.assertEqual(True, False)  # add assertion here
+    def testUpdateStatus(self):
+        self.assertEqual(True, False)  # add assertion here
+    def test__str__(self):
         self.assertEqual(True, False)  # add assertion here
 
 
